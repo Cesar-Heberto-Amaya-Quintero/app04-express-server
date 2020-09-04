0a2c34c9f67504a7ac0a2d71f2378b706ab425fe
Cesar-Heberto-Amaya-Quintero <amaya.cesar.cbtis37@gmail.com>
2020-09-04 10:49:33 -0700

    post-get

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app =express();
 
 const port = 5000;
 
-app.use('/',(request,response)=>{
-    response.send(`<h1> Hello my custom server </h1>`);
-})
+app.use(bodyParser.json()); //consumir cosas
+
+const tacos = [
+    {
+        id: 1,
+        name: 'de asada',
+        quantity: 5,
+        pica: 'no',
+    },
+    {
+        id: 2,
+        name: 'al pastor',
+        quantity: 4,
+        pica: 'yes',
+    },
+    {
+        id: 3,
+        name: 'cabeza',
+        quantity: 6,
+        pica: 'no',
+    },
+]
+
+//Metodo GET o obtener
+app.get('/',(request,response)=>{
+    response.send(tacos);
+});
+
+app.post('/', (request,response)=>{
+    const taco = request.body;
+    const {name,quantity,pica} = request.body;
+    taco.id = tacos.length +1;
+    tacos.push(taco);
+    response.send(taco);
+    //response.send({tag: `Llevate ${quantity} unidades tan solo por ${price} pesos`})
+});
 
 app.listen(port, ()=> console.log(`Server started at: http://localhost:${port}`));
