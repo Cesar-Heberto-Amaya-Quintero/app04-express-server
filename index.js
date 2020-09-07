760fd66c8137f9999b39e475bd81f7cfdfdc252b
Cesar-Heberto-Amaya-Quintero <amaya.cesar.cbtis37@gmail.com>
2020-09-07 15:14:56 -0700

    funcion-delete

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 5000;
 
 app.use(bodyParser.json()); //consumir cosas
 
-const tacos = [
+let tacos = [
     {
         id: 1,
         name: 'de asada',
@@ -29,10 +29,25 @@ const tacos = [
 ]
 
 //Metodo GET o obtener
+//READ
 app.get('/',(request,response)=>{
     response.send(tacos);
 });
 
+app.get('/:id', (request,response)=>{
+    const {id} = request.params;
+    const taco = tacos.find(taco => taco.id == id);
+    response.send(taco);
+});
+
+/* app.get('/:name', (request, response) =>{
+    const {name} = request.params;
+    const taco = tacos.find(taco => taco.name == name);
+    response.send(taco);
+}); */
+
+//agregar taco
+//CREATE
 app.post('/', (request,response)=>{
     const taco = request.body;
     const {name,quantity,pica} = request.body;
@@ -40,6 +55,28 @@ app.post('/', (request,response)=>{
     tacos.push(taco);
     response.send(taco);
     //response.send({tag: `Llevate ${quantity} unidades tan solo por ${price} pesos`})
+});
+
+//UPDATE
+app.put('/:id', (request, response) =>{
+    //buscamos lo que vamos a actualizar
+    const {id} = request.params;
+    const taco = tacos.find(taco => taco.id == id);
+    //pides los cambias al body
+    const {name, quantity, pica} = request.body;
+    //aplicar los cambios
+    taco.name = name;
+    taco.quantity = quantity;
+    taco.pica = pica;
+    //mostrar los cambios
+    response.send(taco);
+});
+
+//DELETE
+app.delete ('/:id', (request,response)=>{
+    const {id} = request.params;
+    tacos = tacos.filter(taco=> taco.id !=id);
+    response.send(tacos);
 });
 
 app.listen(port, ()=> console.log(`Server started at: http://localhost:${port}`));
